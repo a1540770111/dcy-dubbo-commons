e7751c29524d6e164dabb756b418500e3677c1a4
dcy <1540770111@qq.com>
2020-02-18 15:40:26 +0800

    优化代码

--- dcy-db-spring-boot-stater/src/main/java/com/dcy/db/base/model/PageModel.java
+++ dcy-db-spring-boot-stater/src/main/java/com/dcy/db/base/model/PageModel.java
@@ -1,6 +1,7 @@
 package com.dcy.db.base.model;
 
 import com.baomidou.mybatisplus.annotation.TableField;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 /**
@@ -14,21 +15,25 @@ public class PageModel {
      * 当前页面
      */
     @TableField(exist = false)
+    @ApiModelProperty(value = "当前页面")
     private int current = 1;
     /**
      * 每页多少条
      */
     @TableField(exist = false)
+    @ApiModelProperty(value = "每页多少条")
     private int size = 30;
     /**
      * 排序字段
      */
     @TableField(exist = false)
+    @ApiModelProperty(value = "排序字段")
     private String sort;
     /**
      * 排序类型
      */
     @TableField(exist = false)
+    @ApiModelProperty(value = "排序类型")
     private String order;
 
 }
